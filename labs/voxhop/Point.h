31a90b720cfa367d00724af5395687a8a53473b3
martin <mingtai@ucsb.edu>
2024-06-08 13:16:39 -0700

    1

--- labs/voxhop/Point.h
+++ labs/voxhop/Point.h
@@ -7,6 +7,10 @@ class Point {
 public:
     double x, y, z;
 
+    // Default constructor
+    Point() : x(0), y(0), z(0) {}
+
+    // Parameterized constructor
     Point(double x, double y, double z) : x(x), y(y), z(z) {}
 
     bool operator==(const Point& other) const {
@@ -15,6 +19,12 @@ public:
 
     bool operator!=(const Point& other) const {
         return !(*this == other);
+    }
+
+    bool operator<(const Point& other) const {
+        if (x != other.x) return x < other.x;
+        if (y != other.y) return y < other.y;
+        return z < other.z;
     }
 
     // Declare the stream operators
