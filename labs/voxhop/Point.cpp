0a9fef7601b262aee56e1553479c07851e17ea7f
martin <mingtai@ucsb.edu>
2024-06-08 13:15:32 -0700

    1

--- labs/voxhop/Point.cpp
+++ labs/voxhop/Point.cpp
@@ -1,9 +1,15 @@
 #include "Point.h"
+#include <istream>
+#include <ostream>
 
+// Operator to read a Point from an input stream
 std::istream& operator>>(std::istream& stream, Point& point) {
-    return stream >> point.x >> point.y >> point.z;
+    stream >> point.x >> point.y >> point.z;
+    return stream;
 }
 
+// Operator to write a Point to an output stream
 std::ostream& operator<<(std::ostream& stream, const Point& point) {
-    return stream << '(' << point.x << ", " << point.y << ", " << point.z << ')';
+    stream << point.x << " " << point.y << " " << point.z;
+    return stream;
 }
--- labs/voxhop/Point.h
+++ labs/voxhop/Point.h
@@ -1,6 +1,8 @@
 #ifndef POINT_H
 #define POINT_H
 
+#include <iostream>
+
 class Point {
 public:
     double x, y, z;
@@ -14,6 +16,10 @@ public:
     bool operator!=(const Point& other) const {
         return !(*this == other);
     }
+
+    // Declare the stream operators
+    friend std::istream& operator>>(std::istream& stream, Point& point);
+    friend std::ostream& operator<<(std::ostream& stream, const Point& point);
 };
 
 #endif // POINT_H
